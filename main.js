bd5d182c487b526fb06f467b6920ca93845bddd9
NAVEEN KUMAR P <72299336+Naveen-NS67@users.noreply.github.com>
2022-03-03 22:30:03 +0530

    intial commit

---
+++ main.js
@@ -0,0 +1,46 @@
+var navbar = document.getElementById("navbar");
+var menu = document.getElementById("menu");
+
+
+window.onscroll = function(){
+	if (window.pageYOffset >= menu.offsetTop) {
+		navbar.classList.add("sticky");
+	}
+	else{
+		navbar.classList.remove("sticky");
+	}
+}
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 8000); // Change image every 2 seconds
+}
